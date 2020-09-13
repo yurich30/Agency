a49f493f297a0be383a2456f29e80fb93c225bb6
Yurii <yurich3098@gmail.com>
2020-09-13 09:19:02 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-    
+    $('.slider__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+        nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+        infinite:false
+    })
 });
