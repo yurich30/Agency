097278ca5f13127d2f28d5bf773ba11c6e31ac3c
Yurii <yurich3098@gmail.com>
2020-09-17 08:45:58 +0300

    add news section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(function(){
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__slider-inner').slick({
         prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
         nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
         infinite:false
     });
 
     $('select').styler()
+
+    // $('.news__slider-inner').slick({
+    //     prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+    //     nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+    //     infinite:false
+    // });
 });
