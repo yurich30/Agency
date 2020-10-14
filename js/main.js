f9f2545edc6179445bd78bfc832de5ba0269f62d
Yurii <yurich3098@gmail.com>
2020-10-14 17:50:10 +0300

    add adaptive for tablet and mobile

--- js/main.js
+++ js/main.js
@@ -7,9 +7,7 @@ $(function(){
 
     $('select').styler()
 
-    // $('.news__slider-inner').slick({
-    //     prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
-    //     nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
-    //     infinite:false
-    // });
+    $('.header__btn-menu').on('click', function(){
+        $('.menu ul').slideToggle();
+    });
 });
